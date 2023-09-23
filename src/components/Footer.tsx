8a3b1afbc7fca48fd2aed77d5c1ff6f5004165a2
Lucas Castro <lucasamonrc@gmail.com>
2023-09-23 03:39:45 -0500

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,10 @@
-import Link from "next/link";
-
 export function Footer() {
   return (
     <footer className="mt-32 border-t border-zinc-200 md:px-12 px-4 lg:px-24 pt-10 pb-16 flex flex-col md:flex-row justify-between items-center">
       <nav>
         <ul className="flex">
-          <li className="pr-3 py-2 font-medium text-zinc-900 cursor-pointer hover:text-sky-600 transition-colors text-sm">
-            <Link href="/about">About</Link>
-          </li>
           <li className="px-3 py-2 font-medium text-zinc-900 cursor-pointer hover:text-sky-600 transition-colors text-sm">
             <a href="https://blog.lucasamonrc.dev">Articles</a>
-          </li>
-          <li className="px-3 py-2 font-medium text-zinc-900 cursor-pointer hover:text-sky-600 transition-colors text-sm">
-            <Link href="/talks">Talks</Link>
           </li>
           <li className="pl-3 py-2 font-medium text-zinc-900 cursor-pointer hover:text-sky-600 transition-colors text-sm">
             <a href="https://github.com/lucasamonrc?tab=repositories">
